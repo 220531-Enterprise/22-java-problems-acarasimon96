8446dedb51d88385fa569ef08c8f34a927e91565
Timothy Bautista <yule2000@live.com>
2022-06-01 18:39:09 -0600

    Completed question #5

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -152,16 +152,15 @@ public class EvaluationService {
     static class TeenNumberChecker {
 
         public static boolean hasTeen(int x, int y, int z) {
-            // TODO Write an implementation for this method declaration
-            return false;
+            return isTeen(x) || isTeen(y) || isTeen(z) ? true : false;
         }
 
         // We can initialize isTeen method first
         // Then pass the parameter to hasTeen method
 
         public static boolean isTeen(int number) {
-            // TODO Write an implementation for this method declaration
-            return false;
+            boolean result = number >= 13 && number <= 19;
+            return result;
         }
     }
 
